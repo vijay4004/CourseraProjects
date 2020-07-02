a362daa4015ae1eb8d64d5eb6cf3b2792961f23a
Vijay Yadav <vijayyadaviitism@gmail.com>
2020-07-02 23:23:25 +0530

    Adding NodeJS project

---
+++ NodeJS/node-mongoose/index.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const Dishes = require('./models/dishes')
+
+const url = 'mongodb://localhost:27017/conFusion'
+const connect = mongoose.connect(url)
+
+connect.then((db)=>{
+    console.group('Connecetd to the server')
+
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'test'
+    })
+
+        .then((dish) => {
+            console.log(dish)
+            return Dishes.findByIdAndUpdate(dish._id,{
+                $set : { description: 'Updated Test'}
+            },{
+                    new: true 
+            }).exec()
+        })
+        .then((dish)=>{
+            console.log(dish)
+            dish.comments.push({
+                rating:5,
+                comment: 'Test Comment',
+                author: 'Tester'
+            })
+            return dish.save()
+        })
+        .then((dish)=>{
+            console.log(dish)
+            return Dishes.remove({})
+        })
+        .then(()=>{
+            return mongoose.connection.close()
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+})
